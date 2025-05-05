424b0b7c210bcdd8c2f145dc55fbb85d2b803d13
antoineweb <antoineweb@gmail.com>
2025-05-05 12:33:01 +0700

    Initial Commit

---
+++ CompoundInterestCalculator/CompoundInterestCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompoundInterestCalculatorApp.swift
+//  CompoundInterestCalculator
+//
+//  Created by Tony on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CompoundInterestCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompoundInterestCalculatorTests/CompoundInterestCalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CompoundInterestCalculatorTests.swift
+//  CompoundInterestCalculatorTests
+//
+//  Created by Tony on 05/05/2025.
+//
+
+import Testing
+
+struct CompoundInterestCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
